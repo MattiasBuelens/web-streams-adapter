c315933df194d62ada715919d6c4776dfe25d047
Mattias Buelens <mattias@buelens.com>
2018-04-22 17:54:45 +0200

    Bundle with Rollup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const rollupTypescript2 = require('rollup-plugin-typescript2');
+
+const pkg = require('./package.json');
+module.exports = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      freeze: false,
+      sourcemap: true,
+      name: 'WebStreamsAdapter'
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      freeze: false,
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    rollupTypescript2({
+      tsconfig: 'src/tsconfig.json'
+    })
+  ]
+};
