105a944a88fd6b1b1b77821a7f74ce0e789fe449
Mattias Buelens <mattias@buelens.com>
2018-04-22 18:58:50 +0200

    Reformat

--- test/wrapping-readable-stream.ts
+++ test/wrapping-readable-stream.ts
@@ -12,51 +12,51 @@ import {
 import { createWrappingReadableSource } from '../';
 
 export function createWrappingReadableStream(baseClass: ReadableStreamConstructor): ReadableStreamConstructor {
-const wrappingClass = class WrappingReadableStream<R = any> extends baseClass {
+  const wrappingClass = class WrappingReadableStream<R = any> extends baseClass {
 
-  constructor(underlyingSource: ReadableStreamUnderlyingSource<R> = {}, { size, highWaterMark }: Partial<QueuingStrategy> = {}, wrapped = false) {
-    if (!wrapped) {
-      const wrappedReadableStream = new baseClass<R>(underlyingSource, { size, highWaterMark });
-      underlyingSource = createWrappingReadableSource(wrappedReadableStream);
-      size = undefined;
-      highWaterMark = 0;
+    constructor(underlyingSource: ReadableStreamUnderlyingSource<R> = {}, { size, highWaterMark }: Partial<QueuingStrategy> = {}, wrapped = false) {
+      if (!wrapped) {
+        const wrappedReadableStream = new baseClass<R>(underlyingSource, { size, highWaterMark });
+        underlyingSource = createWrappingReadableSource(wrappedReadableStream);
+        size = undefined;
+        highWaterMark = 0;
+      }
+
+      super(underlyingSource, { size, highWaterMark });
     }
 
-    super(underlyingSource, { size, highWaterMark });
-  }
+    get locked() {
+      return super.locked;
+    }
 
-  get locked() {
-    return super.locked;
-  }
+    cancel(reason: any) {
+      return super.cancel(reason);
+    }
 
-  cancel(reason: any) {
-    return super.cancel(reason);
-  }
+    getReader(options: { mode: (R extends Uint8Array ? 'byob' : never) }): ReadableStreamBYOBReader;
+    getReader(options?: { mode?: undefined }): ReadableStreamDefaultReader<R>;
+    getReader(options: { mode?: 'byob' | undefined } = {}): ReadableStreamDefaultReader<R> | ReadableStreamBYOBReader {
+      return super.getReader(options as any);
+    }
 
-  getReader(options: { mode: (R extends Uint8Array ? 'byob' : never) }): ReadableStreamBYOBReader;
-  getReader(options?: { mode?: undefined }): ReadableStreamDefaultReader<R>;
-  getReader(options: { mode?: 'byob' | undefined } = {}): ReadableStreamDefaultReader<R> | ReadableStreamBYOBReader {
-    return super.getReader(options as any);
-  }
+    pipeThrough<T = any>(pair: ReadableWritableStreamPair<T, R>, options?: ReadableStreamPipeOptions): ReadableStream<T> {
+      return super.pipeThrough(pair, options);
+    }
 
-  pipeThrough<T = any>(pair: ReadableWritableStreamPair<T, R>, options?: ReadableStreamPipeOptions): ReadableStream<T> {
-    return super.pipeThrough(pair, options);
-  }
+    pipeTo(dest: WritableStream<R>, options: ReadableStreamPipeOptions = {}) {
+      return super.pipeTo(dest, options);
+    }
 
-  pipeTo(dest: WritableStream<R>, options: ReadableStreamPipeOptions = {}) {
-    return super.pipeTo(dest, options);
-  }
+    tee(): [WrappingReadableStream<R>, WrappingReadableStream<R>] {
+      const [branch1, branch2] = super.tee();
 
-  tee(): [WrappingReadableStream<R>, WrappingReadableStream<R>] {
-    const [branch1, branch2] = super.tee();
+      const wrapped1 = new WrappingReadableStream<R>(createWrappingReadableSource<R>(branch1), {}, true);
+      const wrapped2 = new WrappingReadableStream<R>(createWrappingReadableSource<R>(branch2), {}, true);
+      return [wrapped1, wrapped2];
+    }
+  };
 
-    const wrapped1 = new WrappingReadableStream<R>(createWrappingReadableSource<R>(branch1), {}, true);
-    const wrapped2 = new WrappingReadableStream<R>(createWrappingReadableSource<R>(branch2), {}, true);
-    return [wrapped1, wrapped2];
-  }
-};
+  Object.defineProperty(wrappingClass, 'name', { value: 'ReadableStream' });
 
-Object.defineProperty(wrappingClass, 'name', { value: 'ReadableStream' });
-
-return wrappingClass as ReadableStreamConstructor;
+  return wrappingClass as ReadableStreamConstructor;
 }
--- test/wrapping-writable-stream.ts
+++ test/wrapping-writable-stream.ts
@@ -1,36 +1,37 @@
 import {
   QueuingStrategy,
-  WritableStream, WritableStreamConstructor,
+  WritableStream,
+  WritableStreamConstructor,
   WritableStreamDefaultWriter,
   WritableStreamUnderlyingSink
 } from '@mattiasbuelens/web-streams-polyfill';
 import { createWrappingWritableSink } from '../';
 
 export function createWrappingWritableStream(baseClass: WritableStreamConstructor): WritableStreamConstructor {
-const wrappingClass = class WrappingWritableStream<W = any> extends WritableStream<W> {
+  const wrappingClass = class WrappingWritableStream<W = any> extends WritableStream<W> {
 
-  constructor(underlyingSink: WritableStreamUnderlyingSink<W> = {}, { size, highWaterMark }: Partial<QueuingStrategy> = {}) {
-    const wrappedWritableStream = new baseClass<W>(underlyingSink, { highWaterMark: 1 });
-    underlyingSink = createWrappingWritableSink(wrappedWritableStream);
+    constructor(underlyingSink: WritableStreamUnderlyingSink<W> = {}, { size, highWaterMark }: Partial<QueuingStrategy> = {}) {
+      const wrappedWritableStream = new baseClass<W>(underlyingSink, { highWaterMark: 1 });
+      underlyingSink = createWrappingWritableSink(wrappedWritableStream);
 
-    super(underlyingSink, { size, highWaterMark });
-  }
+      super(underlyingSink, { size, highWaterMark });
+    }
 
-  get locked() {
-    return super.locked;
-  }
+    get locked() {
+      return super.locked;
+    }
 
-  abort(reason: any) {
-    return super.abort(reason);
-  }
+    abort(reason: any) {
+      return super.abort(reason);
+    }
 
-  getWriter(): WritableStreamDefaultWriter<W> {
-    return super.getWriter();
-  }
+    getWriter(): WritableStreamDefaultWriter<W> {
+      return super.getWriter();
+    }
 
-};
+  };
 
-Object.defineProperty(wrappingClass, 'name', { value: 'WritableStream' });
+  Object.defineProperty(wrappingClass, 'name', { value: 'WritableStream' });
 
-return wrappingClass;
+  return wrappingClass;
 }
