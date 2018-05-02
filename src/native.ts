857728b55ae6dbc64695c877c12736a7163d5bb2
Mattias Buelens <mattias@buelens.com>
2018-05-02 22:32:50 +0200

    More types

--- src/native.ts
+++ src/native.ts
@@ -1,5 +1,11 @@
 import { isReadableStreamConstructor, isTransformStreamConstructor, isWritableStreamConstructor } from './checks';
 
+declare global {
+  const ReadableStream: any;
+  const WritableStream: any;
+  const TransformStream: any;
+}
+
 export const NativeReadableStream = typeof ReadableStream === 'function' ? ReadableStream : undefined;
 export const NativeWritableStream = typeof WritableStream === 'function' ? WritableStream : undefined;
 export const NativeTransformStream = typeof TransformStream === 'function' ? TransformStream : undefined;
