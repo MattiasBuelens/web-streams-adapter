c49210680bc82dfcbf7ce67be94e8b578c78a6a3
Mattias Buelens <mattias@buelens.com>
2018-05-11 17:02:44 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export * from './stream-like';
+export * from './wrappers';
+
 export { createReadableStreamWrapper, createWrappingReadableSource } from './readable-wrapper';
 export { createWritableStreamWrapper, createWrappingWritableSink } from './writable-wrapper';
 export { createTransformStreamWrapper, createWrappingTransformer } from './transform-wrapper';
