8afa1bae7c67ef1afa4069283c2333123db334f9
Mattias Buelens <mattias@buelens.com>
2018-06-27 22:26:44 +0200

    Remove useless AssertionError class

--- src/assert.ts
+++ src/assert.ts
@@ -1,8 +1,5 @@
-export class AssertionError extends Error {
-}
-
 export default function assert(test: boolean): void | never {
   if (!test) {
-    throw new AssertionError();
+    throw new TypeError('Assertion failed');
   }
 }
