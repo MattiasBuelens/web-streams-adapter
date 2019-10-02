d76e3789d67b1ab3c91699ecc0c42bde897d2298
Mattias Buelens <mattias@buelens.com>
2019-10-02 22:30:45 +0200

    Fix type

--- test/run-web-platform-tests.ts
+++ test/run-web-platform-tests.ts
@@ -36,7 +36,7 @@ function filter(testPath: string): boolean {
 // https://github.com/w3c/testharness.js/commit/7716e2581a86dfd9405a9c00547a7504f0c7fe94
 // So we emulate it with Node.js events
 const rejections = new Map<Promise<any>, Error>();
-process.on('unhandledRejection', (reason: Error, promise: Promise<any>) => {
+process.on('unhandledRejection', (reason: any, promise: Promise<any>) => {
   rejections.set(promise, reason);
 });
 
