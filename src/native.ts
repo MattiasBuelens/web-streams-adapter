b64d9cff8b99a48ea90c8eee2e55b643395dbb58
Mattias Buelens <mattias@buelens.com>
2021-05-12 01:13:57 +0200

    Remove unused file

--- src/native.ts
+++
@@ -1,9 +0,0 @@
-import { isReadableStreamConstructor, isTransformStreamConstructor, isWritableStreamConstructor } from './checks';
-
-export const NativeReadableStream = typeof ReadableStream === 'function' ? ReadableStream : undefined;
-export const NativeWritableStream = typeof WritableStream === 'function' ? WritableStream : undefined;
-export const NativeTransformStream = typeof TransformStream === 'function' ? TransformStream : undefined;
-
-export const hasNativeReadableStreamConstructor = isReadableStreamConstructor(NativeReadableStream);
-export const hasNativeWritableStreamConstructor = isWritableStreamConstructor(NativeWritableStream);
-export const hasNativeTransformStreamConstructor = isTransformStreamConstructor(NativeTransformStream);
